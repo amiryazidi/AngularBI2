a5624df619804513f0da22fdf1bd405df15e8f2c
Amir YAZIDI <amir.yazidi1@esprit.tn>
2024-02-19 13:42:23 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+import { ResidenceComponent } from './residence/residence.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'product',component:ProductComponent},
+  {path:'residence',component:ResidenceComponent},
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'product/:id',component:DetailProductComponent},
+  {path:'**', component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
 import { ResidenceComponent } from './residence/residence.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductComponent,
-    ResidenceComponent
+    ResidenceComponent,
+    NotFoundComponent,
+    DetailProductComponent
   ],
   imports: [
     BrowserModule,
